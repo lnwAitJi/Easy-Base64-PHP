1dd3e9bfe3d952883251d8d19b2b252882779aa3
Inw AitJi <100911929+lnwAitJi@users.noreply.github.com>
2022-06-10 18:48:50 +0700

    Easy PHP Base64 Encode and Decode

---
+++ base64.php
@@ -0,0 +1,36 @@
+<?php
+//* Add Readline Function *//
+function readline($prompt = ""):string{
+    if($prompt){
+        if(is_array($prompt)){
+            var_dump($prompt);
+        }else {
+            echo $prompt."";
+        }
+    }
+    $fp = fopen("php://stdin","r");
+    $line = rtrim(fgets($fp, 1024));
+    if(empty($line)){
+        return '';
+    }
+    return $line;    
+}
+// ------------------------------------ \\
+#Start!! (～￣▽￣)～
+echo("--- > Start!!< ---\n");                                 # Nothing Just Start #
+
+Back:                                                       # Main Warp #
+$Input = readline("Enter For Encode > ");                   # Input! #
+if ($Input === "") {                                        # If Input = "" #
+    echo("Sorry But it Not Working for a only space\n");    # Sorry Text #
+    goto Back;                                              # Goto Warp #
+} else {
+}
+
+// --- This is a Encode and Decode --- \\
+$encode = base64_encode($Input);                            # Encode #
+$decode = base64_decode($encode);                           # Decode #
+echo($Input." = Encode > ".$encode."\n");                   # Echo Encode #
+echo($encode." = DeCode > ".$decode."\n");                   # Echo Decode #
+
+echo("--- > Done!!< ---\n");                                  # Nothing Just Done #
